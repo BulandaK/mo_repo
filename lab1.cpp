fc8e65d695789e30f8821123e93ad8c0b05274bc
Bulanda <kamilbulandus@gmail.com>
2024-04-22 12:39:48 +0200

    dodanie lab1

---
+++ lab1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <cfloat>
+
+using namespace std;
+void zbadaj_float();
+void zbadaj_double();
+int main() {
+	zbadaj_float();
+	cout << endl;
+	zbadaj_double();
+	return 0;
+}
+void zbadaj_float() {
+	cout << "Float" << endl;
+	float x = 1.0f;
+	float eps = 1.0f;
+	int mantysa = 0;
+	while (x + (eps / 2.0f) > x) {
+		eps /= 2.0f;
+		mantysa++;
+	}
+	cout << "Mantysa wynosi " << mantysa << endl;
+	cout << "Epsilon wynosi " << eps << endl;
+	cout << "Rzeczywista wartosc epsylona maszynowego dla typu float wynosi "
+		<< FLT_EPSILON << endl;
+}
+void zbadaj_double() {
+	cout << "Double" << endl;
+	double x = 1.0;
+	double eps = 1.0;
+	int mantysa = 0;
+	while (x + (eps / 2.0) > x) {
+		eps /= 2.0;
+		mantysa++;
+	}
+	cout << "Mantysa wynosi " << mantysa << endl;
+	cout << "Epsilon wynosi " << eps << endl;
+	cout << "Rzeczywista wartosc epsylona maszynowego dla typu double wynosi "
+		<< DBL_EPSILON << endl;
+}
